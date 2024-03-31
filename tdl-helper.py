100e9790a46c72af2841b7186b4e926afb5802a6
Exodos <thisuraplochana@gmail.com>
2024-03-31 01:09:22 -0500

    Added a new entry to the gitignore file. Clarified a comment on the helper script. Added to a comment on the helper script to tell future me something

--- tdl-helper.py
+++ tdl-helper.py
@@ -4,16 +4,19 @@ import json, os
 
 '''
 This is meant to be a helper script meant to add some features for the tapas-dl.py script
-It's in a seperate file because I don't want to touch the original script 
-(Though I might edit the original to accomadate some features this adds)
+I will edit the original script to assist with the additional functionality I'm adding
+However I wish for the original script to run independently when necessary and the helper script to simply help it along
 
 This script will try to implement the following:
  - A Json based database to keep track of downloaded comics/novels
- - Auto organization of images retrived from the website
+ - Auto organization of images retrived from the website 
+    - Such as putting them in folders organised by chapter number
  - Option to create cbz files from the images
  - Option to provide a file with links the script needs to get chapters from
  - Option to check for updates since last run
-    - And do so without downloading the chapters (will have to look into if this is possible)
+    - And do so without downloading the chapters 
+        - Will have to look into if this is possible
+        - Seems possible, will have to save the pageOffset or the pageCount in the database
 '''
 
 def loadConfig(configJsonPath):
